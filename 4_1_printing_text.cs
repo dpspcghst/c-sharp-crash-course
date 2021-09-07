163f33172605d7e158c701cc5841a5021a5a4f67
Hemingway <ghostblac@protonmail.com>
2021-09-07 10:30:46 -0400

    Update 4_1_printing_text.cs

--- 4_1_printing_text.cs
+++ 4_1_printing_text.cs
@@ -1,1 +1,16 @@
-placeholder
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
