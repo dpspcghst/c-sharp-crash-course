cdf01bb840ddf14fc76bc8470f54fe9067f3c610
Hemingway <ghostblac@protonmail.com>
2021-09-07 10:37:27 -0400

    Update 4_1_printing_text.cs

--- 4_1_printing_text.cs
+++ 4_1_printing_text.cs
@@ -11,6 +11,14 @@ namespace SoloLearn
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+            int the_answer = 42;
+            Console.WriteLine(the_answer);
+            
+            int the_answer = 42;
+            double aspect_ratio = 1.6375;
+            
+            Console.WriteLine("The answer = {0}; aspect ratio = {1}", the_answer, aspect_ratio);
         }
     }
 }
