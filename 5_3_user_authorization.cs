a10ac659ccb999dcbc3ca9f1540399fef814dd4f
Hemingway <ghostblac@protonmail.com>
2021-09-08 22:49:03 -0400

    Create 5_3_user_authorization.cs

---
+++ 5_3_user_authorization.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deepspaceghost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            
+            Console.WriteLine("Name: " + name);
+            
+            string age = Console.ReadLine();
+            
+            Console.WriteLine("Age: " + age);
+        }
+    }
+}
