838ccd85a19c1e39f82d0a1d0b84f1c7be602bde
Hemingway <ghostblac@protonmail.com>
2021-09-08 22:37:22 -0400

    Create 5_2_welcome_message.cs

---
+++ 5_2_welcome_message.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sololearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+
+            name = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}. Welcome to our event.", name);
+        }
+    }
+}
