50f12a06adc2246dc93bb492cc0e639288d1aaa8
Hemingway <ghostblac@protonmail.com>
2021-09-08 22:53:33 -0400

    Create 6_2_C_sharp_is_the_best.cs

---
+++ 6_2_C_sharp_is_the_best.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deepspaceghost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Unnecessary line");
+            Console.WriteLine("C# is the best");
+        }
+    }
+}
