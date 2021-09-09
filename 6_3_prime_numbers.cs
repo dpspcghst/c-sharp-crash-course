f7f7dc66b2b71b6ccd54cf94ae6dff94dd80911c
Hemingway <ghostblac@protonmail.com>
2021-09-08 23:02:40 -0400

    Create 6_3_prime_numbers.cs

---
+++ 6_3_prime_numbers.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deepspaceghost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Uncomment the necessary lines
+            // Console.WriteLine(1);
+            Console.WriteLine(2);
+            Console.WriteLine(5);
+            // Console.WriteLine(8);
+            Console.WriteLine(13);
+            // Console.WriteLine(14);
+            // Console.WriteLine(16);
+            Console.WriteLine(17);
+        }
+    }
+}
