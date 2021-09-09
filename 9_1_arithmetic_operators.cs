824db89e6ddde744c74795a5d2b9cef89f2dad95
Hemingway <ghostblac@protonmail.com>
2021-09-08 22:02:21 -0400

    Update 9_1_arithmetic_operators.cs

--- 9_1_arithmetic_operators.cs
+++ 9_1_arithmetic_operators.cs
@@ -1,4 +1,19 @@
-int num1 = 10;
-int num2 = 4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-Console.WriteLine(num1 - num2);
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 4;
+
+            Console.WriteLine(num1 - num2);
+        }
+    }
+}
